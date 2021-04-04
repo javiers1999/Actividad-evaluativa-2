dbf39ba721558506e7b95ff09047b0d90bac8998
javiers1999 <56318901+javiers1999@users.noreply.github.com>
2021-04-04 15:39:54 -0400

    final

--- ts/validarRut.ts
+++ ts/validarRut.ts
@@ -4,8 +4,8 @@ function checkRut(rut) {
     // Despejar Guión
     valor = valor.replace('-', '');
     // Aislar Cuerpo y Dígito Verificador
-    var cuerpo = valor.slice(0, -1);
-    var dv = valor.slice(-1).toUpperCase();
+    let cuerpo = valor.slice(0, -1);
+    let dv = valor.slice(-1).toUpperCase();
     // Formatear RUN
     rut.value = cuerpo + '-' + dv;
     // Si no cumple con el mínimo ej. (n.nnn.nnn)
@@ -14,12 +14,12 @@ function checkRut(rut) {
         return false;
     }
     // Calcular Dígito Verificador
-    var suma = 0;
-    var multiplo = 2;
+    let suma = 0;
+    let multiplo = 2;
     // Para cada dígito del Cuerpo
-    for (var i = 1; i <= cuerpo.length; i++) {
+    for (let i = 1; i <= cuerpo.length; i++) {
         // Obtener su Producto con el Múltiplo Correspondiente
-        var index = multiplo * valor.charAt(cuerpo.length - i);
+        let index = multiplo * valor.charAt(cuerpo.length - i);
         // Sumar al Contador General
         suma = suma + index;
         // Consolidar Múltiplo dentro del rango [2,7]
@@ -30,16 +30,20 @@ function checkRut(rut) {
             multiplo = 2;
         }
     }
+    
     // Calcular Dígito Verificador en base al Módulo 11
-    var dvEsperado = 11 - (suma % 11);
+    let dvEsperado = 11 - (suma % 11);
+
     // Casos Especiales (0 y K)
     dv = (dv == 'K') ? 10 : dv;
     dv = (dv == 0) ? 11 : dv;
+
     // Validar que el Cuerpo coincide con su Dígito Verificador
     if (dvEsperado != dv) {
         rut.setCustomValidity("RUT Inválido");
         return false;
     }
+
     // Si todo sale bien, eliminar errores (decretar que es válido)
     rut.setCustomValidity('');
 }
